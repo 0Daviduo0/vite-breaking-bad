55519c835a68cba7c667d769536a72e8c9ee978f
Daviduo <davide0400@gmail.com>
2022-12-16 10:02:04 +0100

    adding counter

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -19,6 +19,7 @@ export default{
 
     h1{
         font-weight: 700;
+        color: #fff;
     }
 
 </style>
--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -18,11 +18,15 @@ import { store } from '../store.js';
     <div class="filter-container">
         <form>
             <select name="status" id="status" v-model="store.selectInput" @click.prevent="$emit('search')">
-                <option value="Alive">Alive</option>
-                <option value="Dead">Dead</option>
-                <option value="unknown">unknown</option>
+                <option value="">ALL</option>
+                <option value="Alive">ALIVE</option>
+                <option value="Dead">DEAD</option>
+                <option value="unknown">UNKNOWN</option>
             </select>
         </form>
+        <div class="counter">
+            {{ store.characterList.length }} CHARACTERS FOUNDED
+        </div>
     </div>
 
 
@@ -36,7 +40,27 @@ import { store } from '../store.js';
     .filter-container {
         display: flex;
         justify-content: center;
+        align-items: center;
         padding-bottom: 80px;
+    }
+
+    .counter{
+        background-color: rgba(255, 255, 255, 0.342);
+        padding: 3px 10px;
+        margin-left: 10px;
+        border-radius: 10px;
+        overflow: hidden;
+        color: #fff;
+        font-weight: 700;
+    }
+
+    #status{
+        background-color: rgba(255, 255, 255, 0.342);
+        padding: 3px 10px;
+        border: none;
+        border-radius: 10px;
+        overflow: hidden;
+        font-weight: 700;
     }
 
 </style>
